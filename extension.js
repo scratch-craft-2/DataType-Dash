9303cc99382368a12fa59444219d0e5d5c99b56c
Фёдор <156839189+scratch-craft-2@users.noreply.github.com>
2025-09-08 13:48:38 +0300

    Create extension.js

---
+++ extension.js
@@ -0,0 +1,71 @@
+// Name: DataType
+// ID: Data-New-Types
+// By: scratch_craft_2 <https://scratch.mit.edu/users/scratch_craft_2/>
+// License: MPL-2.0
+
+(function (Scratch) {
+    "use strict";
+
+  if (!Scratch.extensions.unsandboxed) {
+    throw new Error("This Extension must run unsandboxed");
+  }
+
+    class DataType {
+      getInfo() {
+        return {
+          id: " Data-New-Types",
+          name: "DataType",
+          color1: "#59c059",
+          blocks: [
+            {
+              opcode: "TypeSTR",
+              blockType: Scratch.BlockType.REPORTER,
+              text: "Str [valuestr]",
+              arguments: {
+                valuestr: {
+                  defaultValue: "1",
+                  type: Scratch.ArgumentType.STRING,
+                },
+            },
+           },
+          {
+              opcode: "TypeNum",
+              blockType: Scratch.BlockType.REPORTER,
+              text: "Num [valuenum]",
+              arguments: {
+                valuenum: {
+                  defaultValue: "1",
+                  type: Scratch.ArgumentType.STRING,
+                },
+            },
+           },
+           { 
+              opcode: "TypeBool",
+              blockType: Scratch.BlockType.REPORTER,
+              text: "Boolean [valuebool]",
+              arguments: {
+                valuebool: {
+                  defaultValue: "1",
+                  type: Scratch.ArgumentType.STRING,
+                },
+            },
+           },
+          ],
+        };
+      }
+TypeSTR(args) {
+ let str = "([args.valuestr])";
+ return str ;
+}
+TypeNum(args) {
+  return ([args.valuenum] + 0);
+}
+TypeBool(args) {
+  if [args.valuebool] {
+  return True 
+  }
+  return False
+}
+    }
+    Scratch.extensions.register(new modals());
+  })(Scratch);
